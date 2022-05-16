8e52c3434cf2cb7b0552dd60fc979c6aee938430
iwillsky <45908192+Iwillsky@users.noreply.github.com>
2022-05-16 21:36:04 +0800

    Debug0516a

--- IaC-Protein/alphafold2_install.sh
+++ IaC-Protein/alphafold2_install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 yum install epel-release python3 -y
 yum install aria2 -y
+
 yum remove moby-cli.x86_64 moby-containerd.x86_64 moby-engine.x86_64 moby-runc.x86_64 -y
 yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.4.13-3.1.el7.x86_64.rpm
 yum install docker-ce -y
 systemctl --now enable docker
+
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
    && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
 yum clean expire-cache
 yum install -y nvidia-docker2
-#sudo systemctl restart docker
-#sudo usermod -aGdocker $USER
+systemctl restart docker
+#usermod -aGdocker $USER
 #newgrp docker
-#sudo su
+
 cd /opt
 git clone https://github.com/deepmind/alphafold.git
 cd alphafold/
@@ -21,4 +23,4 @@ sed -i '/SHELL ["/bin/bash", "-c"]/a\RUN gpg --keyserver keyserver.ubuntu.com --
 docker build -f docker/Dockerfile -t alphafold .
 pip3 install -r docker/requirements.txt
 
-waagent -deprovision+user -force
+#waagent -deprovision+user -force
